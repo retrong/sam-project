d27c59dc8e98c9972ec47e9b46729bc2aa675c8c
retrong <eguobabarth@gmail.com>
2023-09-13 19:52:34 +0100

    modified loading component and added logo to index.html

--- src/component/organisms/Loading.jsx
+++ src/component/organisms/Loading.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import { loader } from '../../assets'
+import React from 'react';
 
 const Loading = () => {
   return (
-    <div className='flex justify-center align-middle items-center py-32'>
-        <img src={loader} alt="" className='w-[100px] h-[100px]'/>
-    </div>
-  )
+		<div className="flex justify-center align-middle items-center py-32">
+			<div className="loader-spinner"></div>
+		</div>
+	);
 }
 
 export default Loading
